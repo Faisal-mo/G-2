aba553ec40debc1f9e421dd4a0290bf81aa1d9d0
Faisal-mo <faisal57075@gmail.com>
2025-08-18 22:27:08 +0300

    Place towers
    add place tower inside the grids for the 2 players and add upgrade to towers and send mini boss to the opponent

---
+++ G2/Assets/Fiasal/Fscript/FTowerPlacer.cs
@@ -0,0 +1,149 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FTowerPlacer : MonoBehaviour
+{
+    [Header("Tower Prefabs")]
+    public GameObject[] towerPrefabsP1; // Assign 3 towers in Inspector (1,2,3)
+    public GameObject[] towerPrefabsP2; // Assign 3 towers in Inspector (8,9,0)
+
+    [Header("Mini-Boss")]
+    public GameObject miniBossPrefabP1; // Assign in Inspector
+    public GameObject miniBossPrefabP2;
+    public Transform spawnPointP1, spawnPointP2;
+
+    // Player states
+    private int selectedTowerIndexP1 = -1; // -1 = nothing selected
+    private int selectedTowerIndexP2 = -1;
+    private bool isUpgradingP1 = false;
+    private bool isUpgradingP2 = false;
+    private bool isSendingBossP1 = false;
+    private bool isSendingBossP2 = false;
+
+    void Update()
+    {
+        HandlePlayer1Inputs();
+        HandlePlayer2Inputs();
+    }
+
+    // Player 1 Inputs (1-2-3, W, S, Space)
+    void HandlePlayer1Inputs()
+    {
+        // Tower Selection (1,2,3)
+        if (Input.GetKeyDown(KeyCode.Alpha1)) selectedTowerIndexP1 = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) selectedTowerIndexP1 = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) selectedTowerIndexP1 = 2;
+
+        // Upgrade Mode (W)
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            isUpgradingP1 = true;
+            isSendingBossP1 = false;
+            Debug.Log("P1: Select a tower to upgrade!");
+        }
+
+        // Boss Mode (S)
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isSendingBossP1 = true;
+            isUpgradingP1 = false;
+            Debug.Log("P1: Press SPACE to send boss!");
+        }
+
+        // Confirm Action (Space)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isUpgradingP1) UpgradeRandomTower(1);
+            else if (isSendingBossP1) SendMiniBoss(1);
+            else if (selectedTowerIndexP1 != -1) PlaceTower(1, towerPrefabsP1[selectedTowerIndexP1]);
+        }
+    }
+
+    // Player 2 Inputs (8-9-0, O, L, Enter)
+    void HandlePlayer2Inputs()
+    {
+        // Tower Selection (8,9,0)
+        if (Input.GetKeyDown(KeyCode.Alpha8)) selectedTowerIndexP2 = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha9)) selectedTowerIndexP2 = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha0)) selectedTowerIndexP2 = 2;
+
+        // Upgrade Mode (O)
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            isUpgradingP2 = true;
+            isSendingBossP2 = false;
+            Debug.Log("P2: Select a tower to upgrade!");
+        }
+
+        // Boss Mode (L)
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            isSendingBossP2 = true;
+            isUpgradingP2 = false;
+            Debug.Log("P2: Press ENTER to send boss!");
+        }
+
+        // Confirm Action (Enter)
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (isUpgradingP2) UpgradeRandomTower(2);
+            else if (isSendingBossP2) SendMiniBoss(2);
+            else if (selectedTowerIndexP2 != -1) PlaceTower(2, towerPrefabsP2[selectedTowerIndexP2]);
+        }
+    }
+
+    void PlaceTower(int playerID, GameObject prefab)
+    {
+        Vector3 pos = GridManager.Instance.GetRandomBuildPosition(playerID);
+        if (pos != Vector3.zero)
+        {
+            Instantiate(prefab, pos, Quaternion.identity);
+            GridManager.Instance.OccupyCell(pos);
+            Debug.Log($"P{playerID}: Placed tower!");
+        }
+        ResetPlayerState(playerID);
+    }
+
+    void UpgradeRandomTower(int playerID)
+    {
+        // Find all towers owned by this player and upgrade one randomly
+        Tower[] allTowers = FindObjectsOfType<Tower>();
+        List<Tower> playerTowers = new List<Tower>();
+        foreach (Tower t in allTowers)
+        {
+            if (t.ownerID == playerID) playerTowers.Add(t);
+        }
+        if (playerTowers.Count > 0)
+        {
+            Tower randomTower = playerTowers[Random.Range(0, playerTowers.Count)];
+            randomTower.Upgrade();
+            Debug.Log($"P{playerID}: Upgraded a tower!");
+        }
+        ResetPlayerState(playerID);
+    }
+
+    void SendMiniBoss(int playerID)
+    {
+        Transform spawnPoint = (playerID == 1) ? spawnPointP1 : spawnPointP2;
+        GameObject prefab = (playerID == 1) ? miniBossPrefabP1 : miniBossPrefabP2;
+        Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+        Debug.Log($"P{playerID}: Sent mini-boss!");
+        ResetPlayerState(playerID);
+    }
+
+    void ResetPlayerState(int playerID)
+    {
+        if (playerID == 1)
+        {
+            selectedTowerIndexP1 = -1;
+            isUpgradingP1 = false;
+            isSendingBossP1 = false;
+        }
+        else
+        {
+            selectedTowerIndexP2 = -1;
+            isUpgradingP2 = false;
+            isSendingBossP2 = false;
+        }
+    }
+}
